8d73d483af6602b6e853b41ae196464f8f3b4fa8
optigo <amrut9979466052@gmail.com>
2023-10-02 19:00:13 +0530

    push1

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,96 @@
+//QR Code Scanner Sample1
+
 // import 'package:flutter/material.dart';
+// import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-// void main() {
-//   runApp(const MainApp());
-// }
 
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
+// void main() => runApp(QRCodeScannerApp());
 
+// class QRCodeScannerApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: Text('Hello World!'),
-//         ),
-//       ),
+//     return MaterialApp(
+//       title: 'QR Code Scanner',
+//       home: QRCodeScannerScreen(),
 //     );
 //   }
 // }
 
+// class QRCodeScannerScreen extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() => QRCodeScannerScreenState();
+// }
+
+// class QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
+//   GlobalKey qrKey = GlobalKey();
+//   late QRViewController controller;
+//   bool scanning = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title:const Text("QR Code Scanner"),
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             flex: 5,
+//             child: QRView(
+//               key: qrKey,
+//               onQRViewCreated: _onQRViewCreated,
+//             ),
+//           ),
+//           Expanded(
+//             flex: 1,
+//             child: Center(
+//               child: Text(scanning ? 'Scanning...' : 'Scan a QR Code'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   void _onQRViewCreated(QRViewController controller) {
+//     this.controller = controller;
+//     controller.scannedDataStream.listen((scanData) {
+//       setState(() {
+//         scanning = true;
+//       });
+//       // Handle the scanned QR code data here
+//       print('Scanned data: ${scanData.code}');
+      
+//       // You can perform any action you want with the scanned data.
+//       // For example, navigate to a new screen or display it in a dialog.
+      
+//       // After processing the data, stop scanning for a moment to avoid multiple scans
+//       Future.delayed(Duration(seconds: 2), () {
+//         if (mounted) {
+//           setState(() {
+//             scanning = false;
+//           });
+//         }
+//       });
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     controller.dispose();
+//     super.dispose();
+//   }
+// }
+
+// QR Code Scanner Sample 2
+
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
-
 
 void main() => runApp(QRCodeScannerApp());
 
@@ -54,7 +123,15 @@ class QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text("QR Code Scanner"),
+        title: const Text('QR Code Scanner'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.flash_on),
+            onPressed: () {
+              // Implement flashlight toggle here
+            },
+          ),
+        ],
       ),
       body: Column(
         children: <Widget>[
@@ -72,6 +149,12 @@ class QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
             ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Implement manual input of QR code data
+        },
+        child: const Icon(Icons.keyboard),
       ),
     );
   }
@@ -105,4 +188,5 @@ class QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
     super.dispose();
   }
 }
+
 
